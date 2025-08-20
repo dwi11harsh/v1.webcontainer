548c5f1badab36266050e77eb85fad6ca74260f7
Prabhat Dwivedi <harsh@Prabhats-MacBook-Air.local>
2025-08-21 01:15:20 +0530

    adding function signatures

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use std::collections::HashMap;
 
 use wasm_bindgen::prelude::*;
@@ -43,5 +44,95 @@ impl VirtualFS {
 
     pub fn all_files(&self) -> String {
         format!("Current virtual directory: {}", self.current_dir)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Directory {
+    // name of this directory (not the full path)
+    pub name: String,
+    // optional parent directory none for root)
+    pub parent: Option<Box<Directory>>,
+    // child directories by name
+    pub children: HashMap<String, Directory>,
+    // files in this directory by name
+    pub files: HashMap<String, Vec<u8>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum FileType {
+    Dir,
+    File,
+}
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum DeleteOption {
+    R,
+}
+
+pub struct GrepMatch {
+    pub file_path: String,
+    pub line_number: usize,
+    pub line_content: String,
+}
+
+impl Directory {
+    // constructor
+    pub fn mkdir(name: String, parent: Option<Box<Directory>>) -> Self {
+        Directory {
+            name,
+            parent,
+            children: HashMap::new(),
+            files: HashMap::new(),
+        }
+    }
+
+    pub fn ls(&self) -> HashMap<FileType, String> {
+        let mut list: HashMap<FileType, String> = HashMap::new();
+
+        // directory names
+        for (name, _) in &self.children {
+            list.insert(FileType::Dir, name.clone());
+        }
+
+        // file names
+        for (name, _) in &self.files {
+            list.insert(FileType::File, name.clone());
+        }
+        list
+    }
+
+    pub fn rm(&self, name: String, flag: Option<DeleteOption>) -> Result<(), String> {
+        match flag {
+            Some(flg) => {
+                // delete directory
+                Ok(())
+            }
+            None => {
+                // delete file
+                Ok(())
+            }
+        }
+    }
+
+    pub fn grep(&self, pattern: String) -> Result<Vec<GrepMatch>, String> {
+        Ok(Vec::new())
+    }
+
+    // this will actually return &String
+    pub fn cat(&self) -> String {
+        "this will return the content of specified file".to_string()
+    }
+
+    // another cat method but this one can take multiple strings (filenames) and then combine them
+    pub fn cat_multiple(&self, filenames: Vec<String>) -> Result<String, String> {
+        Ok("combined content".to_string())
+    }
+
+    pub fn head(&self, file_name: String) -> Result<String, String> {
+        Ok("dummy head".to_string())
+    }
+
+    pub fn tail(&self, file_name: String) -> Result<String, String> {
+        Ok("dummy tail".to_string())
     }
 }
